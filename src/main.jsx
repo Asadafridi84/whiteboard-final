feb6ffe2cecea50fb96fa1ff1480d0f048dd732a
Asadafridi84 <gm.for.bu@gmail.com>
2026-01-21 11:34:12 +0100

    Change import extension for Whiteboard component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 ﻿import React from "react"
 import ReactDOM from "react-dom/client"
-import Whiteboard from "./components/Whiteboard-simple."
+import Whiteboard from "./components/Whiteboard-simple.jsx"
 import "./index.css"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
